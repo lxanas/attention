247f626f22c8ad1280569b9c88fb3f75e4d112b4
Kyle Lee <57109450+lxanas@users.noreply.github.com>
2022-09-01 09:39:17 +0800

    Add files via upload

---
+++ W01Practice.py
@@ -0,0 +1,22 @@
+# aa = 'My first Python program'
+# print(aa)
+# print("hello world")
+# a = 4
+# a += 1
+# print(a)
+# a = 2.3
+# b = 2.4
+# c = 2.5
+# d = 2.6
+# print(round(a), round(b), round(c), round(d))
+
+earning = 2665565
+invest = 1000000
+roi = (earning - invest) / invest
+print(round(roi, 3))
+print("hello", end=" 1")
+print("world")
+print("111")
+print("01234567890123456789")
+print("rank".ljust(5), "player".ljust(5))
+print("1".center(4))
