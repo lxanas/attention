b102a16eefb8c356e446f8eda8485e2441062bae
kyle <912525577@qq.com>
2022-09-01 09:44:14 +0800

    finish WK1

--- W01/WK1_Q2.py
+++ W01/WK1_Q2.py
@@ -3,13 +3,13 @@ SPY = float(input("Enter the amount invested in SPY: "))
 QQQ = float(input("Enter the amount invested in QQQ: "))
 EEM = float(input("Enter the amount invested in EEM: "))
 GGG = float(input("Enter the amount invested in GGG: "))
-sum = SPY + QQQ + EEM + GGG
-shareS = SPY / sum
-shareQ = QQQ / sum
-shareE = EEM / sum
-shareG = GGG / sum
+count = SPY + QQQ + EEM + GGG
+shareS = SPY / count
+shareQ = QQQ / count
+shareE = EEM / count
+shareG = GGG / count
 print("{0:<6s}{1:<6.2%}".format("SPY", shareS))
 print("{0:<6s}{1:<6.2%}".format("QQQ", shareQ))
 print("{0:<6s}{1:<6.2%}".format("EEM", shareE))
 print("{0:<6s}{1:<6.2%}".format("GGG", shareG))
-print("TOTAL AMOUNT INVESTED for {0:s}: ${1:10,.2f}.".format(ID, sum))
+print("TOTAL AMOUNT INVESTED for {0:s}: ${1:10,.2f}.".format(ID, count))
