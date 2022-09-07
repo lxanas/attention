404acd92ca9e290a675ae82ff1ebeb4c4405ec14
lxanas <912525577@qq.com>
2022-09-07 21:50:59 +0800

    WK2class

---
+++ W01/practice1.py
@@ -0,0 +1,4 @@
+string = input("enter a sentence:")
+replace = input("enter a word replace:")
+replacement = input("enter a replacement:")
+print(string.replace(replace, replacement))
---
+++ W01/practice2.py
@@ -0,0 +1,6 @@
+cents = int(input("cents:"))
+# Quarters = cents // 25
+# Dimes = (cents - Quarters * 25) // 10
+# Nickels = (cents - Quarters * 25 - Dimes * 10) // 5
+# cent = cents - Quarters * 25 - Dimes * 10 - Nickels * 5
+# print("hello")
