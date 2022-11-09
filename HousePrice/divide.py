fc2bb9139a45a38789715a8e73af565026184647
kyle <912525577@qq.com>
2022-11-09 18:05:55 +0800

    try

--- HousePrice/divide.py
+++ HousePrice/divide.py
@@ -7,7 +7,7 @@ raw = open('data_for_analysis.csv', 'r')
 raw = [i.rstrip() for i in raw]
 # print(raw[:5])
 raw = raw[1:]
-head = "block group ID,median house value,median income,housing median age,total rooms,total bedrooms,population,households,latitude,longitude\n"
+head = "block group ID,median house value,median income,housing median age,total rooms,total bedrooms,population,households,latitude,longitude,ocean_proximity\n"
 train.write(head)
 test.write(head)
 for i in raw:
--- HousePrice/model.py
+++ HousePrice/model.py
@@ -2,7 +2,7 @@ import pandas as pd
 import tensorflow as tf
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Dense, LSTM
+from keras.layers import Dense, LSTM, Dropout
 from matplotlib import pyplot as plt
 
 
@@ -14,17 +14,22 @@ def read_data(file):
     numeric_features = raw_data.dtypes[raw_data.dtypes != 'object'].index
     # print(numeric_features)
 
-    numeric_features = numeric_features[1:]
+    house_price = numeric_features[0]
+    loc = numeric_features[7:9]
+    numeric_features = numeric_features[1:7]
+    print(house_price)
     # print(numeric_features)
     raw_data[numeric_features] = raw_data[numeric_features].apply(lambda x: (x - x.mean()) / (x.std()))  # z-score
+    # raw_data[house_price] = raw_data[house_price].apply(lambda x: np.log(x))
+    raw_data[loc] = raw_data[loc].apply(lambda x: np.log(abs(x)))
+    raw_data[loc] = raw_data[loc].apply(lambda x: (x - x.mean()) / (x.std()))
     # raw_data.info()
-    # print(raw_data[:5])
+    print(raw_data[:5])
     # print(type(numeric_features))
     ocean = list(set(raw_data['ocean_proximity']))
     # print(ocean)
     raw_data = pd.get_dummies(raw_data)
     return raw_data
-
 
 train = read_data("train.csv")
 test = read_data("test.csv")
@@ -52,19 +57,24 @@ print(y.shape)
 
 def create_model():
     model = Sequential()
-
+    model.add(LSTM(128, input_shape=(1, 13), return_sequences=True))
+    model.add(Dropout(0.2))
     model.add(Dense(128, input_shape=(13,), activation='relu', name='dense_1'))
+    model.add(Dropout(0.2))
     model.add(Dense(64, activation='relu', name='dense_2'))
+    model.add(Dropout(0.1))
     model.add(Dense(32, activation='relu', name='dense_3'))
-    model.add(Dense(1, activation='linear', name='dense_output'))
+    model.add(Dense(1, name='dense_output'))
 
-    model.compile(optimizer='adam', loss='mae', metrics=['mse'])
+    model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mse'])
     return model
 
 
 # print(x[:3])
-# x = x.reshape(-1, 1, 13)
-# y = y.reshape(-1, 1, 1)
+x = x.reshape(-1, 1, 13)
+y = y.reshape(-1, 1, 1)
+x_test = x_test.reshape(-1, 1, 13)
+y_test = y_test.reshape(-1, 1, 1)
 print(x.shape)
 print(y.shape)
 # print(x[:3])
